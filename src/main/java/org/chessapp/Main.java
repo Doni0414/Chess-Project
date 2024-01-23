8cf636c759bc003a4f4ddf435238a5c357d08170
Doni0414 <nurda.agabek@mail.ru>
2024-01-24 00:55:39 +0600

    Display knights

--- src/main/java/org/chessapp/Main.java
+++ src/main/java/org/chessapp/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.chessapp.piece.Knight;
 import org.chessapp.piece.Pawn;
 
 public class Main extends Application {
@@ -18,6 +19,18 @@ public class Main extends Application {
             Coordinate coordinate = new Coordinate(i, 6);
             board.setPiece(new Pawn(Color.WHITE, coordinate), coordinate);
         }
+
+        //Display black Knights
+        Coordinate knightCoordinate1 = new Coordinate(1, 0);
+        board.setPiece(new Knight(Color.BLACK, knightCoordinate1), knightCoordinate1);
+        Coordinate knightCoordinate2 = new Coordinate(6, 0);
+        board.setPiece(new Knight(Color.BLACK, knightCoordinate1), knightCoordinate2);
+
+        //Display white Knights
+        Coordinate knightCoordinate3 = new Coordinate(1, 7);
+        board.setPiece(new Knight(Color.BLACK, knightCoordinate3), knightCoordinate3);
+        Coordinate knightCoordinate4 = new Coordinate(6, 7);
+        board.setPiece(new Knight(Color.BLACK, knightCoordinate4), knightCoordinate4);
 
         Scene scene = new Scene(board, Configuration.W, Configuration.H);
         stage.setScene(scene);
