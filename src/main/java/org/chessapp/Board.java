8f3f0575a844de8798d8d283cde028a9df01f149
Doni0414 <nurda.agabek@mail.ru>
2024-01-24 00:17:38 +0600

    Board and Cell classes are updated

--- src/main/java/org/chessapp/Board.java
+++ src/main/java/org/chessapp/Board.java
@@ -2,20 +2,30 @@ package org.chessapp;
 
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 public class Board extends GridPane {
+    private Cell[][] cells;
     public Board(double width, double height){
+        this.cells = new Cell[8][8];
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 Cell cell;
                 if ((i + j) % 2 == 0){
-                    cell = new Cell(width / 8, height / 8, Color.WHITE);
+                    cell = new Cell(width / 8, height / 8, Color.WHITE, new Coordinate(j, i));
                 }else{
-                    cell = new Cell(width / 8, height / 8, Color.BLACK);
+                    cell = new Cell(width / 8, height / 8, Color.BLACK, new Coordinate(j, i));
                 }
+                cells[i][j] = cell;
                 add(cell, j, i);
             }
         }
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public void setCells(Cell[][] cells) {
+        this.cells = cells;
     }
 }
--- src/main/java/org/chessapp/Cell.java
+++ src/main/java/org/chessapp/Cell.java
@@ -6,10 +6,28 @@ import javafx.scene.shape.Rectangle;
 
 public class Cell extends StackPane {
     private Rectangle rectangle;
+    private Coordinate coordinate;
 
-    public Cell(double width, double height, Color color){
+    public Cell(double width, double height, Color color, Coordinate coordinate){
         this.rectangle = new Rectangle(width, height);
         this.rectangle.setFill(color);
         getChildren().add(rectangle);
+        setCoordinate(coordinate);
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public void setRectangle(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
     }
 }
