3a5acc16096a58201cdd97a11dafc034de36dfe3
Doni0414 <nurda.agabek@mail.ru>
2024-01-24 22:57:46 +0600

    Knight movements are added

--- src/main/java/org/chessapp/piece/Knight.java
+++ src/main/java/org/chessapp/piece/Knight.java
@@ -2,8 +2,11 @@ package org.chessapp.piece;
 
 import javafx.scene.paint.Color;
 import org.chessapp.Board;
+import org.chessapp.Cell;
 import org.chessapp.Coordinate;
+import org.chessapp.ValidMove;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Knight extends Piece{
@@ -19,11 +22,36 @@ public class Knight extends Piece{
 
     @Override
     public List<Coordinate> getMoves(Board board) {
-        return null;
+        List<Coordinate> coordinates = new ArrayList<>();
+        int x = coordinate.getX(), y = coordinate.getY();
+        int[] dx = {-2, -2, 2, 2, -1, 1, -1, 1};
+        int[] dy = {-1, 1, -1, 1, -2, -2, 2, 2};
+        for (int i = 0; i < dx.length; i++) {
+            Coordinate newCoordinate = new Coordinate(x + dx[i], y + dy[i]);
+            if (ValidMove.isValidMove(newCoordinate)){
+                if (board.isEmptyCell(newCoordinate)){
+                    coordinates.add(newCoordinate);
+                }
+            }
+        }
+        return coordinates;
     }
 
     @Override
     public List<Coordinate> getEatMoves(Board board) {
-        return null;
+        List<Coordinate> coordinates = new ArrayList<>();
+        int x = coordinate.getX(), y = coordinate.getY();
+        int[] dx = {-2, -2, 2, 2, -1, 1, -1, 1};
+        int[] dy = {-1, 1, -1, 1, -2, -2, 2, 2};
+        for (int i = 0; i < dx.length; i++) {
+            Coordinate newCoordinate = new Coordinate(x + dx[i], y + dy[i]);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece != null && piece.isBlack() != isBlack()){
+                    coordinates.add(newCoordinate);
+                }
+            }
+        }
+        return coordinates;
     }
 }
