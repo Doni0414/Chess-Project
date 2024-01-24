959acbddc66ee7e4fdb0989d624a68bfb043de14
Doni0414 <nurda.agabek@mail.ru>
2024-01-24 22:50:35 +0600

    Rook movements are added

--- src/main/java/org/chessapp/piece/Rook.java
+++ src/main/java/org/chessapp/piece/Rook.java
@@ -2,8 +2,11 @@ package org.chessapp.piece;
 
 import javafx.scene.paint.Color;
 import org.chessapp.Board;
+import org.chessapp.Cell;
 import org.chessapp.Coordinate;
+import org.chessapp.ValidMove;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece{
@@ -19,11 +22,107 @@ public class Rook extends Piece{
 
     @Override
     public List<Coordinate> getMoves(Board board) {
-        return null;
+        List<Coordinate> coordinates = new ArrayList<>();
+        int x = coordinate.getX(), y = coordinate.getY();
+        for (int i = x - 1; i >= 0; i--) {
+            Coordinate newCoordinate = new Coordinate(i, y);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece == null){
+                    coordinates.add(newCoordinate);
+                }else{
+                    break;
+                }
+            }
+        }
+        for (int i = x + 1; i < 8; i++) {
+            Coordinate newCoordinate = new Coordinate(i, y);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece == null){
+                    coordinates.add(newCoordinate);
+                }else{
+                    break;
+                }
+            }
+        }
+        for (int i = y - 1; i >= 0; i--) {
+            Coordinate newCoordinate = new Coordinate(x, i);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece == null){
+                    coordinates.add(newCoordinate);
+                }else{
+                    break;
+                }
+            }
+        }
+        for (int i = y + 1; i < 8; i++) {
+            Coordinate newCoordinate = new Coordinate(x, i);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece == null){
+                    coordinates.add(newCoordinate);
+                }else{
+                    break;
+                }
+            }
+        }
+        return coordinates;
     }
 
     @Override
     public List<Coordinate> getEatMoves(Board board) {
-        return null;
+        List<Coordinate> coordinates = new ArrayList<>();
+        int x = coordinate.getX(), y = coordinate.getY();
+        for (int i = x - 1; i >= 0; i--) {
+            Coordinate newCoordinate = new Coordinate(i, y);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece != null){
+                    if (piece.isBlack() != isBlack()) {
+                        coordinates.add(newCoordinate);
+                    }
+                    break;
+                }
+            }
+        }
+        for (int i = x + 1; i < 8; i++) {
+            Coordinate newCoordinate = new Coordinate(i, y);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece != null){
+                    if (piece.isBlack() != isBlack()) {
+                        coordinates.add(newCoordinate);
+                    }
+                    break;
+                }
+            }
+        }
+        for (int i = y - 1; i >= 0; i--) {
+            Coordinate newCoordinate = new Coordinate(x, i);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece != null){
+                    if (piece.isBlack() != isBlack()) {
+                        coordinates.add(newCoordinate);
+                    }
+                    break;
+                }
+            }
+        }
+        for (int i = y + 1; i < 8; i++) {
+            Coordinate newCoordinate = new Coordinate(x, i);
+            if (ValidMove.isValidMove(newCoordinate)){
+                Piece piece = board.getCell(newCoordinate).getPiece();
+                if (piece != null){
+                    if (piece.isBlack() != isBlack()) {
+                        coordinates.add(newCoordinate);
+                    }
+                    break;
+                }
+            }
+        }
+        return coordinates;
     }
 }
